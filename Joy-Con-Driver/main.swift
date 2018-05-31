c23866e9fea7781b972bf725b76eaa5dee9ec4a7
hukurou-s <s18g455@stu.kagawa-u.ac.jp>
2018-05-31 19:14:13 +0900

    implements the function to receive input from Joy-Con

--- Joy-Con-Driver/main.swift
+++ Joy-Con-Driver/main.swift
@@ -9,15 +9,39 @@
 import Foundation
 import IOKit.hid
 
-var JoyConR: IOHIDDevice? = nil
-//static let target = JoyCon()
-//let manager = IOHIDManagerCreate(nil, 0)
-let manager = IOHIDManagerCreate(kCFAllocatorDefault, IOOptionBits(kIOHIDOptionsTypeNone))
+let manager = IOHIDManagerCreate(kCFAllocatorDefault, 0)
 let matching = [kIOHIDVendorIDKey: 0x057E, kIOHIDProductIDKey: 0x2007]
 
 IOHIDManagerSetDeviceMatching(manager, matching as CFDictionary?)
 
+let matchingCallback: IOHIDDeviceCallback = {context, result, sender, device in
+
+    IOHIDDeviceOpen(device, 1)
+    IOHIDDeviceScheduleWithRunLoop(device, CFRunLoopGetCurrent(), CFRunLoopMode.defaultMode.rawValue)
+
+    let reportCallback : IOHIDReportCallback = { context, result, sender, type, reportId, report, reportLength in
+        let data = Data(bytes: report, count: reportLength)
+        print(data.map { String(format: "%.2hhx", $0) }.joined())
+    }
+
+    let report = UnsafeMutablePointer<UInt8>.allocate(capacity: 4)
+
+    IOHIDDeviceRegisterInputReportCallback(device, report, 4, reportCallback, nil)
+}
+
+
+IOHIDManagerRegisterDeviceMatchingCallback(manager, matchingCallback, nil)
+
+let removeCallback: IOHIDDeviceCallback = {context, result, sender, device in
+    // do something
+}
+IOHIDManagerRegisterDeviceRemovalCallback(manager, removeCallback, nil)
+
 IOHIDManagerScheduleWithRunLoop(manager, CFRunLoopGetCurrent(), CFRunLoopMode.defaultMode.rawValue)
 let result = IOHIDManagerOpen(manager, 1)
-
 print(result)
+
+CFRunLoopRun()
+
+IOHIDManagerClose(manager, 0)
+IOHIDManagerUnscheduleFromRunLoop(manager, CFRunLoopGetMain(), CFRunLoopMode.defaultMode.rawValue)
