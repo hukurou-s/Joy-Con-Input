3a63cf808d237630da631018842c911d469e9020
hukurou-s <s18g455@stu.kagawa-u.ac.jp>
2018-06-02 23:47:24 +0900

    WIP Commit

---
+++ Joy-Con-Driver/JoyConButton.swift
@@ -0,0 +1,20 @@
+//
+// Created by 石井怜央 on 2018/06/03.
+// Copyright (c) 2018 LEO. All rights reserved.
+//
+
+import Foundation
+
+enum JoyConButton: String {
+    case A = "3f010008"
+    case B = "3f040008"
+    case X = "3f020008"
+    case Y = "3f080008"
+    case R = "3f004008"
+    case ZR = "3f008008"
+    case Plus = "3f000208"
+    case Home = "3f001008"
+    case SR = "3f200008"
+    case SL = "3f100008"
+    case Neutral = "3f000008"
+}
--- Joy-Con-Driver/main.swift
+++ Joy-Con-Driver/main.swift
@@ -9,10 +9,76 @@
 import Foundation
 import IOKit.hid
 
+/*func buttonCheck(buttonCode: String) -> JoyConButton {
+    var button = JoyConButton.Neutral
+
+    switch buttonCode {
+    case "3f010008":
+        button = JoyConButton.A
+    case "3f040008":
+        button = JoyConButton.B
+    case "3f020008":
+        button = JoyConButton.X
+    case "3f080008":
+        button = JoyConButton.Y
+    case "3f004008":
+        button = JoyConButton.R
+    case "3f008008":
+        button = JoyConButton.ZR
+    case "3f000208":
+        button = JoyConButton.Plus
+    case "3f001008":
+        button = JoyConButton.Home
+    case "3f200008":
+        button = JoyConButton.SR
+    case "3f100008":
+        button = JoyConButton.SL
+    default:
+        break // do nothing
+
+    }
+
+    return button
+
+}*/
+
 let manager = IOHIDManagerCreate(kCFAllocatorDefault, 0)
 let matching = [kIOHIDVendorIDKey: 0x057E, kIOHIDProductIDKey: 0x2007]
 
 IOHIDManagerSetDeviceMatching(manager, matching as CFDictionary?)
+
+func buttonCheck(buttonCode: String) -> JoyConButton {
+    var button = JoyConButton.Neutral
+
+    switch buttonCode {
+    case "3f010008":
+        button = JoyConButton.A
+    case "3f040008":
+        button = JoyConButton.B
+    case "3f020008":
+        button = JoyConButton.X
+    case "3f080008":
+        button = JoyConButton.Y
+    case "3f004008":
+        button = JoyConButton.R
+    case "3f008008":
+        button = JoyConButton.ZR
+    case "3f000208":
+        button = JoyConButton.Plus
+    case "3f001008":
+        button = JoyConButton.Home
+    case "3f200008":
+        button = JoyConButton.SR
+    case "3f100008":
+        button = JoyConButton.SL
+    default:
+        break // do nothing
+
+    }
+
+    return button
+
+}
 
 let matchingCallback: IOHIDDeviceCallback = {context, result, sender, device in
 
@@ -21,33 +87,12 @@ let matchingCallback: IOHIDDeviceCallback = {context, result, sender, device in
 
     let reportCallback : IOHIDReportCallback = { context, result, sender, type, reportId, report, reportLength in
         let data = Data(bytes: report, count: reportLength)
-        let string = data.map { String(format: "%.2hhx", $0) }.joined()
+        let buttonCode = String(data.map { String(format: "%.2hhx", $0) }.joined())
+        print(buttonCode)
 
-        switch string {
-            case "3f010008":
-                print("A is pressed")
-            case "3f040008":
-                print("B is pressed")
-            case "3f020008":
-                print("X is pressed")
-            case "3f080008":
-                print("Y is pressed")
-            case "3f004008":
-                print("Z is pressed")
-            case "3f008008":
-                print("ZR is pressed")
-            case "3f000208":
-                print("+ is pressed")
-            case "3f001008":
-                 print("home is pressed")
-            case "3f200008":
-                print("SR is pressed")
-            case "3f100008":
-                print("SL is pressed")
-            default:
-                break // do nothing
+        let button: JoyConButton = buttonCheck(buttonCode)
 
-        }
+        print(button)
     }
 
     let report = UnsafeMutablePointer<UInt8>.allocate(capacity: 4)
