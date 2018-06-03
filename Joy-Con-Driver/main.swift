c9d4dd77f4e200b15ca5d19aea8cf10dfb09dc0f
hukurou-s <s18g455@stu.kagawa-u.ac.jp>
2018-06-03 23:53:30 +0900

    Refactor using enum

--- Joy-Con-Driver/main.swift
+++ Joy-Con-Driver/main.swift
@@ -9,44 +9,6 @@
 import Foundation
 import IOKit.hid
 
-/*func buttonCheck(buttonCode: String) -> JoyConButton {
-    var button = JoyConButton.Neutral
-
-    switch buttonCode {
-    case "3f010008":
-        button = JoyConButton.A
-    case "3f040008":
-        button = JoyConButton.B
-    case "3f020008":
-        button = JoyConButton.X
-    case "3f080008":
-        button = JoyConButton.Y
-    case "3f004008":
-        button = JoyConButton.R
-    case "3f008008":
-        button = JoyConButton.ZR
-    case "3f000208":
-        button = JoyConButton.Plus
-    case "3f001008":
-        button = JoyConButton.Home
-    case "3f200008":
-        button = JoyConButton.SR
-    case "3f100008":
-        button = JoyConButton.SL
-    default:
-        break // do nothing
-
-    }
-
-    return button
-
-}*/
-
-let manager = IOHIDManagerCreate(kCFAllocatorDefault, 0)
-let matching = [kIOHIDVendorIDKey: 0x057E, kIOHIDProductIDKey: 0x2007]
-
-IOHIDManagerSetDeviceMatching(manager, matching as CFDictionary?)
-
 func buttonCheck(buttonCode: String) -> JoyConButton {
     var button = JoyConButton.Neutral
 
@@ -80,6 +42,11 @@ func buttonCheck(buttonCode: String) -> JoyConButton {
 
 }
 
+let manager = IOHIDManagerCreate(kCFAllocatorDefault, 0)
+let matching = [kIOHIDVendorIDKey: 0x057E, kIOHIDProductIDKey: 0x2007]
+
+IOHIDManagerSetDeviceMatching(manager, matching as CFDictionary?)
+
 let matchingCallback: IOHIDDeviceCallback = {context, result, sender, device in
 
     IOHIDDeviceOpen(device, 1)
@@ -88,9 +55,8 @@ let matchingCallback: IOHIDDeviceCallback = {context, result, sender, device in
     let reportCallback : IOHIDReportCallback = { context, result, sender, type, reportId, report, reportLength in
         let data = Data(bytes: report, count: reportLength)
         let buttonCode = String(data.map { String(format: "%.2hhx", $0) }.joined())
-        print(buttonCode)
 
-        let button: JoyConButton = buttonCheck(buttonCode)
+        let button: JoyConButton = buttonCheck(buttonCode: buttonCode)
 
         print(button)
     }
